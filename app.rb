9c75420c26396ee0cbe4619aa74ea54d37da0991
buggiescode <137247769+buggiescode@users.noreply.github.com>
2025-04-23 02:49:11 +0000

    Finished RPS

--- app.rb
+++ app.rb
@@ -2,8 +2,60 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  
+  erb(:rules)
+end
+
+get("/rock") do
+  @your_move = "rock"
+
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @result = "tied"
+  elsif @comp_move == "paper"
+    @result = "lost"
+  elsif @comp_move == "scissors"
+    @result = "won"
+  end
+
+  erb(:game)
+end
+
+get("/paper") do
+  @your_move = "paper"
+
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @result = "won"
+  elsif @comp_move == "paper"
+    @result = "tied"
+  elsif @comp_move == "scissors"
+    @result = "lost"
+  end
+
+  erb(:game)
+end
+
+get("/scissors") do
+  @your_move = "scissors"
+
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @result = "lost"
+  elsif @comp_move == "paper"
+    @result = "won"
+  elsif @comp_move == "scissors"
+    @result = "tied"
+  end
+
+  erb(:game)
 end
